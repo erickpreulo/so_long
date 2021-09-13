bd249532514e8c770559b375809b5ec44c35fe7c
Erick Gomes <egomes@c2r4s6.42lisboa.com>
2021-09-13 20:09:58 +0100

    start42

--- so_long.c
+++ so_long.c
@@ -10,33 +10,61 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
-#include <stdio.h>
+#include "so_long.h"
 
-#define UP 126 
-#define DOWN 125 
+int x;
+int y;
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
-int	key_hook(int keycode, t_vars *vars)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	// printf("Hello from key_hook! Key: %d\n", keycode);
-	if (keycode == UP)
-		printf("UP\n");
-	if (keycode == DOWN)
-		printf("DOWN\n");
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
+
 
 int	main(void)
 {
-	t_vars	vars;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+	int i;
+	int j;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_loop(vars.mlx);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 400, 400, "Hello world!");
+	img.img = mlx_new_image(mlx, 400, 400);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+	&img.endian);
 
+	// i = -1;
+	// while (++i < 400)
+	// {
+	// 	j = -1;
+	// 	while (++j < 400)
+	// 		my_mlx_pixel_put(&img, i, j, 0x00ffffff);
+	// }
+
+	x = 180;
+	y = 150;
+
+	i = x;
+	while (++i < x + 50)
+	{
+		j = y;
+		while (++j < y + 50)
+			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+	}
+
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
